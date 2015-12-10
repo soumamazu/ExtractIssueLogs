7a9873ca67586d238dbca315a04194d58f4710d7
soummazu <soummazu@SOUMMAZU-LAP.oradev.oraclecorp.com>
2015-12-10 20:43:11 +0530

    Deploy Commit

--- src/java/ExtractData/Logs.java
+++ src/java/ExtractData/Logs.java
@@ -46,23 +46,23 @@ public class Logs extends HttpServlet
                 out.println("<table border=\"1\">");
                 
                 out.println("<tr>");
-                out.println("</td>Total number of open issues :</td>");
+                out.println("<td>Total number of open issues :</td>");
                 out.println("<td>"+el.total_open+"</td>");
                 out.println("</tr>");
                 
                 out.println("<tr>");
                 out.println("<td>Number of open issues that were opened in the last 24 hours :</td>");
-                out.println("</td>"+el.last_24+"</td>");
+                out.println("<td>"+el.last_24+"</td>");
                 out.println("</tr>");
                 
                 out.println("<tr>");
                 out.println("<td>Number of open issues that were opened more than 24 hours ago but less than 7 days ago :</td>");
-                out.println("</td>"+el.more_24_less_7+"</td>");
+                out.println("<td>"+el.more_24_less_7+"</td>");
                 out.println("</tr>");
                 
                 out.println("<tr>");
                 out.println("<td>Number of open issues that were opened more than 7 days ago :</td>");
-                out.println("</td>"+el.more_7+"</td>");
+                out.println("<td>"+el.more_7+"</td>");
                 out.println("</tr>");                
                 
                 out.println("<table>");
